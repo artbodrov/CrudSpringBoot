4f6e67805b697763efa6dde544d85a1866437993
artbodrov <artem.bodrov@gmail.com>
2022-08-09 17:42:20 +0300

    Initial commit

---
+++ src/test/java/h8me/CrudSpringBoot/CrudSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package h8me.CrudSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
